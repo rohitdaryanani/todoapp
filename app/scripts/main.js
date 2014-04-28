007618040ef19f5ae6fbb36b08822e14d4d534c4
Rohit Daryanani <rohit.daryanani@globalzeal.net>
2014-04-28 13:30:22 +0800

    Implemented Add Todo Items
    - Created event handler Object for events
    - Added Custome event enterTodo
    - Implemented Add Todo event

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+    var Todo = {
+        init: function() {
+            this.bindEvents();
+        },
+
+        bindEvents: function() {
+        	var writeSomething = $('#write-something');
+
+            writeSomething.on('enterKey', this.addTodo);
+            writeSomething.on('keypress', this.enterTodo);
+        },
+
+        addTodo: function(e) {
+            var value = $(this).val();
+
+            e.preventDefault();
+            $('<li id="todo-list"><input type="checkbox"><span>' + value + '</span><button class="destroy">Delete</button></li>').appendTo('#todos');
+            $(this).val('');
+        },
+
+        enterTodo: function(e) {
+            if (e.keyCode === 13 && $(this)) {
+                $(this).trigger('enterKey');
+                e.preventDefault();
+            };
+        }
+    };
+
+    Todo.init();
